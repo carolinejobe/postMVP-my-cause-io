0dc7c50061efd45b4e1f724602bba20d9c60d25c
carolinejobe <caroline.h.jobe@gmail.com>
2016-03-07 15:35:37 -0500

    initial commit of AddUser controller

---
+++ src/io/mycause/controller/AddUser.java
@@ -0,0 +1,42 @@
+package io.mycause.controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AddUser {
+
+		@RequestMapping("/testAddUser")
+		public ModelAndView addUser(@RequestParam("groupName") String groupName, @RequestParam("email") String email) {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				Connection c;
+				String connectionString = "jdbc:mysql://localhost:3306/sakila";
+				c = DriverManager.getConnection(connectionString, "root", "admin");
+				
+				String insertStatement = "INSERT INTO 'sakila'.'actor'('first_name','last_name')VALUES(?,?)";
+				
+				Statement s = c.createStatement();
+				PreparedStatement insertPreparedStatement = c.prepareStatement(insertStatement);
+				insertPreparedStatement.setString(1, groupName);
+				insertPreparedStatement.setString(2, email);
+				
+				ResultSet results = insertPreparedStatement.executeQuery();
+						
+				return new ModelAndView("testShowUser", "newUser", insertPreparedStatement);
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+				return new ModelAndView("error", "errorMessage", e.getMessage());
+			}
+		}		
+}
