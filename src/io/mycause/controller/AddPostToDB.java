bfd01877c6046185c086bb0735ccc5a78fb68346
carolinejobe <caroline.h.jobe@gmail.com>
2016-03-08 14:53:05 -0500

    updating request params

--- src/io/mycause/controller/AddPostToDB.java
+++ src/io/mycause/controller/AddPostToDB.java
@@ -3,34 +3,34 @@ package io.mycause.controller;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.Statement;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AddPostToDB {
 
+	//need a way to identify the user making the post (behind the scenes?) so that we associate the correct foreign key with the post
+	
 	@RequestMapping("/postSuccess")
-	public String addUser(@RequestParam("firstname") String firstname, @RequestParam("lastname") String lastname,
-			@RequestParam("email") String email, @RequestParam("password") String password) {
+	public String addPost(@RequestParam("primarycategory") String primarycategory, @RequestParam("secondarycategory") String secondarycategory,
+			@RequestParam("description") String description, @RequestParam("location") String location) {
 		try {
-			// create connection to mySQL database
+			// create connection to mySQL database- this will change when we get AWS db built
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection c;
 			String connectionString = "jdbc:mysql://localhost:3306/mycauseio";
 			c = DriverManager.getConnection(connectionString, "root", "admin");
 
 			// create mySQL insert statement
-			String insertStatement = "INSERT INTO mycauseio.users(firstname,lastname, email, password)VALUES(?,?,?,?)";
+			String insertStatement = "INSERT INTO mycauseio.post(primarycategory,secondarycategory, description, location)VALUES(?,?,?,?)";
 
 			// create the mySQL insert preparedstatement
 			PreparedStatement insertPreparedStatement = c.prepareStatement(insertStatement);
-			insertPreparedStatement.setString(1, firstname);
-			insertPreparedStatement.setString(2, lastname);
-			insertPreparedStatement.setString(3, email);
-			insertPreparedStatement.setString(4, password);
+			insertPreparedStatement.setString(1, primarycategory);
+			insertPreparedStatement.setString(2, secondarycategory);
+			insertPreparedStatement.setString(3, description);
+			insertPreparedStatement.setString(4, location);
 
 			// execute the preparedstatement
 			insertPreparedStatement.execute();
