18625317bc620737d23a17215bbe2197fb3f1ba6
Jeseekia Vaughn <jeseekia@gmail.com>
2016-03-08 11:31:15 -0500

    Specifying the class as a controller and adding route mapping for the cause page.

--- src/io/mycause/controller/ShowCause.java
+++ src/io/mycause/controller/ShowCause.java
@@ -1,5 +1,17 @@
 package io.mycause.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class ShowCause {
+	
+	@RequestMapping("/cause")
+	public ModelAndView helloWorld()
+	{
+	return new
+	ModelAndView("cause","message","Test Cause Message");
+	}
 
 }
