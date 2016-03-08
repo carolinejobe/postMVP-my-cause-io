3da53555b27e035b08048ef3474ce7cf6f2b6dfb
carolinejobe <caroline.h.jobe@gmail.com>
2016-03-08 09:59:28 -0500

    updated @requestParams to match form fields

--- src/io/mycause/controller/AddUser.java
+++ src/io/mycause/controller/AddUser.java
@@ -12,32 +12,34 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class AddUser {
 
-		@RequestMapping("/signup")
-		public ModelAndView addUser(@RequestParam("groupName") String groupName, @RequestParam("email") String email) {
-			try {
-				//create connection to mySQL database
-				Class.forName("com.mysql.jdbc.Driver");
-				Connection c;
-				String connectionString = "jdbc:mysql://localhost:3306/sakila";
-				c = DriverManager.getConnection(connectionString, "root", "admin");
-				
-				//create mySQL insert statement
-				String insertStatement = "INSERT INTO 'sakila'.'actor'('first_name','last_name')VALUES(?,?)";
-				
-				// create the mySQL insert preparedstatement
-				PreparedStatement insertPreparedStatement = c.prepareStatement(insertStatement);
-				insertPreparedStatement.setString(1, groupName);
-				insertPreparedStatement.setString(2, email);
-				
-				// execute the preparedstatement
-				insertPreparedStatement.execute();
-				
-						
-				return new ModelAndView("testShowUser", "newUser", insertPreparedStatement);
-				
-			} catch (Exception e) {
-				e.printStackTrace();
-				return new ModelAndView("error", "errorMessage", e.getMessage());
-			}
-		}		
+	@RequestMapping("/signup")
+	public ModelAndView addUser(@RequestParam("firstname") String firstname, @RequestParam("lastname") String lastname,
+			@RequestParam("email") String email, @RequestParam("password") String password) {
+		try {
+			// create connection to mySQL database
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection c;
+			String connectionString = "jdbc:mysql://localhost:3306/sakila";
+			c = DriverManager.getConnection(connectionString, "root", "admin");
+
+			// create mySQL insert statement
+			String insertStatement = "INSERT INTO 'sakila'.'actor'('first_name','last_name')VALUES(?,?)";
+
+			// create the mySQL insert preparedstatement
+			PreparedStatement insertPreparedStatement = c.prepareStatement(insertStatement);
+			insertPreparedStatement.setString(1, firstname);
+			insertPreparedStatement.setString(2, lastname);
+			insertPreparedStatement.setString(3, email);
+			insertPreparedStatement.setString(4, password);
+
+			// execute the preparedstatement
+			insertPreparedStatement.execute();
+
+			return new ModelAndView("testShowUser", "newUser", insertPreparedStatement);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ModelAndView("error", "errorMessage", e.getMessage());
+		}
+	}
 }
