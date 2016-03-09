bd1254f69a691ba5cb3cc9bed41739e9219781a5
carolinejobe <caroline.h.jobe@gmail.com>
2016-03-09 14:55:24 -0500

    changing connection code

--- src/io/mycause/controller/AddUser.java
+++ src/io/mycause/controller/AddUser.java
@@ -3,6 +3,11 @@ package io.mycause.controller;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,17 +19,26 @@ public class AddUser {
 	public String addUser(@RequestParam("firstname") String firstname, @RequestParam("lastname") String lastname,
 			@RequestParam("email") String email, @RequestParam("password") String password, @RequestParam("organization") String organization) {
 		try {
-			// create connection to mySQL database
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection c;
-			String connectionString = "jdbc:mysql://localhost:3306/mycauseio";
-			c = DriverManager.getConnection(connectionString, "root", "admin");
-
+//			// create connection to mySQL database
+//			Class.forName("com.mysql.jdbc.Driver");
+//			Connection conn;
+//			String connectionString = "jdbc:mysql://localhost:3306/mycauseio";
+//			conn = DriverManager.getConnection(connectionString, "root", "admin");
+////////////
+			Context ctx = new InitialContext();
+			DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/dbb");
+			String connectionUrl = "jdbc:mysql://mycauseio.c8vanjxufcpj.us-west-2.rds.amazonaws.com:3306";
+			Connection conn = ds.getConnection(); 
+	//		Statement st= conn.createStatement(); 
+////////////			
+			
 			// create mySQL insert statement
 			String insertStatement = "INSERT INTO mycauseio.users(firstname,lastname, email, password, organization)VALUES(?,?,?,?,?)";
 
 			// create the mySQL insert preparedstatement
-			PreparedStatement insertPreparedStatement = c.prepareStatement(insertStatement);
+			PreparedStatement insertPreparedStatement = conn.prepareStatement(insertStatement);
+			
+			
 			insertPreparedStatement.setString(1, firstname);
 			insertPreparedStatement.setString(2, lastname);
 			insertPreparedStatement.setString(3, email);
