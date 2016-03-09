c52cb9192bbdbf2475058eaeb8e7e244236f50ad
carolinejobe <caroline.h.jobe@gmail.com>
2016-03-09 12:05:42 -0500

    updated prepared statement to reflect new db columns

--- src/io/mycause/controller/AddPostToDB.java
+++ src/io/mycause/controller/AddPostToDB.java
@@ -10,27 +10,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AddPostToDB {
 
-	//need a way to identify the user making the post (behind the scenes?) so that we associate the correct foreign key with the post
-	
 	@RequestMapping("/postSuccess")
-	public String addPost(@RequestParam("primarycategory") String primarycategory, @RequestParam("secondarycategory") String secondarycategory,
-			@RequestParam("description") String description, @RequestParam("location") String location) {
+	public String addPost(@RequestParam("category") String category, @RequestParam("postheadline") String postheadline,
+			@RequestParam("post_desc") String post_desc) {
 		try {
-			// create connection to mySQL database- this will change when we get AWS db built
+			// create connection to mySQL database- this will change when we get
+			// AWS db built
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection c;
-			String connectionString = "jdbc:mysql://localhost:3306/mycauseio";
+			String connectionString = "jdbc:mysql://mycauseio.c8vanjxufcpj.us-west-2.rds.amazonaws.com:3306";
 			c = DriverManager.getConnection(connectionString, "root", "admin");
 
-			// create mySQL insert statement
-			String insertStatement = "INSERT INTO mycauseio.post(primarycategory,secondarycategory, description, location)VALUES(?,?,?,?)";
+			// create mySQL insert statement.
+			// need to update insertStatement to account for jumping with FKs:
+			// user selects a category, need to update post table with that cat_ID.
+			// -->!! also need to populate the user_id based on who is logged in when post is created. 
+			String insertStatement = "INSERT INTO mycauseio.post(post_headline, cat_id, post_desc, )VALUES(?,?,?,?)";
 
 			// create the mySQL insert preparedstatement
 			PreparedStatement insertPreparedStatement = c.prepareStatement(insertStatement);
-			insertPreparedStatement.setString(1, primarycategory);
-			insertPreparedStatement.setString(2, secondarycategory);
-			insertPreparedStatement.setString(3, description);
-			insertPreparedStatement.setString(4, location);
+			insertPreparedStatement.setString(1, category);
+			insertPreparedStatement.setString(2, postheadline);
+			insertPreparedStatement.setString(3, post_desc);
 
 			// execute the preparedstatement
 			insertPreparedStatement.execute();
