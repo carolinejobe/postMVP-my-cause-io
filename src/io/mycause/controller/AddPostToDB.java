4892b3bffc567fa12afba0d5988651555cf5821a
carolinejobe <caroline.h.jobe@gmail.com>
2016-03-15 11:06:57 -0400

    making postcause controller model and view to give message if user gets bounced to login when trying to create a post without being logged in

--- src/io/mycause/controller/AddPostToDB.java
+++ src/io/mycause/controller/AddPostToDB.java
@@ -18,28 +18,22 @@ import org.springframework.web.servlet.ModelAndView;
 public class AddPostToDB {
 
 	@RequestMapping("/postSuccess")
-	public ModelAndView addPost(@RequestParam("category") int category, @RequestParam("postheadline") String postheadline,
-			@RequestParam("post_desc") String post_desc, HttpServletRequest request) {
+	public ModelAndView addPost(@RequestParam("category") int category,
+			@RequestParam("postheadline") String postheadline, @RequestParam("post_desc") String post_desc,
+			HttpServletRequest request) {
 		try {
 			Context ctx = new InitialContext();
 			DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/dbb");
 			Connection conn = ds.getConnection();
 
-			// get user_id from cookie
+			// get user_id from cookie and cast to int
 			Cookie[] clientCookies = request.getCookies();
-			
 			String userID = "";
 			int userIDint;
-			if (clientCookies[1] != null) {
-				userID = clientCookies[1].getValue();
-				userIDint = Integer.parseInt(userID);
-			} else {
-				return new ModelAndView("login", "message", "You must be logged in to create a post.");
-			}
+			userID = clientCookies[1].getValue();
+			userIDint = Integer.parseInt(userID);
 
 			// create mySQL insert statement.
-			// -->!! also need to populate the user_id based on who is logged in
-			// when post is created.
 			String insertStatement = "INSERT INTO maindb.posts(post_headline, post_desc, cat_id, user_id)VALUES(?, ?, ?, ?)";
 
 			// create the mySQL insert preparedstatement
@@ -52,11 +46,11 @@ public class AddPostToDB {
 			// execute the preparedstatement
 			insertPreparedStatement.execute();
 
-			return new ModelAndView ("success", "message", "Your post has been added!");
+			return new ModelAndView("success", "message", "Your post has been added!");
 
 		} catch (Exception e) {
 			e.printStackTrace();
-			return new ModelAndView ("error", "message", "Sorry, something happened. Please go back and try again.");
+			return new ModelAndView("error", "message", "Sorry, something happened. Please go back and try again.");
 		}
 	}
 }
