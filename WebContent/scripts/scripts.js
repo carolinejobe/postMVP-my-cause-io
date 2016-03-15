2320239b8eb3bd97b607ce5c29c08a383d42d069
Jeseekia Vaughn <jeseekia@gmail.com>
2016-03-15 12:10:12 -0400

    Updating the logic to display the user's email and reformat it as email[at]example[dot]com.

--- WebContent/scripts/scripts.js
+++ WebContent/scripts/scripts.js
@@ -1,3 +1,35 @@
 $( "#connect" ).click(function() {
-  alert( "Woo connected!" );
+	
+	var email = document.getElementById("contact").name
+	var res = email.split("@");
+    console.log(res);
+	var res1 = res.slice(0,1);
+    console.log(res1);
+	var res2 = res[1].split(".");
+    console.log(res2);
+	var res3 = res2.slice(1,2);
+    console.log(res3);
+	
+	res2 = res2.slice(0,1);
+    console.log(res2);
+	
+	res1.push("[at]");
+    console.log(res1);
+	
+	res2.push("[dot]");
+    console.log(res2);
+	
+	var newEmailArr = res1.concat(res2).concat(res3);
+    console.log(newEmailArr);
+	
+	var newEmail = " ";
+	
+	newEmailArr.forEach(rejoin);
+	function rejoin(item,index,array) {
+		newEmail += item;
+        console.log(newEmail);
+	}
+	
+	
+	alert("Email "+ newEmail + " to connect to this cause");
 });
